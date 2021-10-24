696f5a22cb71b83eabbb9de199f1972d458fa9e9
Thomas Parry <yrrapt@gmail.com>
2021-10-24 13:36:35 +0200

    Updated bash launching script

--- docker/bash.sh
+++ docker/bash.sh
@@ -1,4 +1,3 @@
-# docker run -v $PWD/..:/cacd -it tudelft-space-cacd /bin/bash
-# docker run --env=DISPLAY --volume=/tmp/.X11-unix:/tmp/.X11-unix --net=host -it tudelft-space-cacd /bin/bash
 xhost +
-docker run --rm -ti --net=host -e DISPLAY=:0 -it tudelft-space-cacd /bin/bash
+docker run --rm -ti --net=host -e DISPLAY=:0 -v $PWD/..:/cacd_host -it tudelft-space-cacd /bin/bash
+
